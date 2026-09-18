20f4ae3edb8b5549b1dbd5b0f9622ef1b231a9d7
RecuperationIntelligente <126275350+RecuperationIntelligente@users.noreply.github.com>
2026-09-18 13:33:24 +0700

    Add files via upload

---
+++ Week 1/KSwiftFun/KSwiftFun/KSwiftFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  KSwiftFunApp.swift
+//  KSwiftFun
+//
+//  Created by MAY 05 on 18/9/26.
+//
+
+import SwiftUI
+
+@main
+struct KSwiftFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
