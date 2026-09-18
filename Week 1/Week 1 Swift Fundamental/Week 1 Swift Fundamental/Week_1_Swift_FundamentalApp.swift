3726c1108a57cf166ef28007301bb43e11571718
RecuperationIntelligente <126275350+RecuperationIntelligente@users.noreply.github.com>
2026-09-18 13:37:33 +0700

    Add files via upload

---
+++ Week 1/Week 1 Swift Fundamental/Week 1 Swift Fundamental/Week_1_Swift_FundamentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week_1_Swift_FundamentalApp.swift
+//  Week 1 Swift Fundamental
+//
+//  Created by MAY 05 on 18/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Week_1_Swift_FundamentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
