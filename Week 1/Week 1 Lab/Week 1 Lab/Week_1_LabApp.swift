fae08cae77ff6cb8ca847149a3f4b1520f94f274
RecuperationIntelligente <126275350+RecuperationIntelligente@users.noreply.github.com>
2026-09-18 13:47:49 +0700

    Add files via upload

---
+++ Week 1/Week 1 Lab/Week 1 Lab/Week_1_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week_1_LabApp.swift
+//  Week 1 Lab
+//
+//  Created by MAY 05 on 18/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Week_1_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
